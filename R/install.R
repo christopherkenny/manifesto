18b3d856efd1b9768082e71a574ac30f5a2d61f6
Christopher Kenny <ctk44@cornell.edu>
2025-05-14 16:51:28 -0400

    style

--- R/install.R
+++ R/install.R
@@ -18,23 +18,41 @@ install_manifest <- function(path = 'rproject.toml', groups = NULL, dry_run = FA
     return(invisible(character()))
   }
 
-  pkg_refs <- purrr::pmap_chr(deps_df, function(package, version, source, repo, ref) {
+  pkg_refs <- vapply(seq_len(nrow(deps_df)), function(i) {
+    pkg <- deps_df$package[i]
+    ver <- deps_df$version[i]
+    source <- deps_df$source[i]
+    repo <- deps_df$repo[i]
+    ref <- deps_df$ref[i]
+
     if (source %in% c('CRAN', 'bioc')) {
-      if (!is.na(version)) paste0(package, '@', version) else package
+      if (!is.na(ver)) {
+        ref_string <- paste0(pkg, '@', ver)
+      } else {
+        ref_string <- pkg
+      }
     } else if (source %in% c('github', 'gitlab')) {
-      ref_part <- if (!is.na(ref)) paste0('@', ref) else ''
-      paste0(repo, ref_part)
+      if (!is.na(ref)) {
+        ref_string <- paste0(repo, '@', ref)
+      } else {
+        ref_string <- repo
+      }
     } else if (source == 'git') {
-      ref_part <- if (!is.na(ref)) paste0('@', ref) else ''
-      paste0('git::', repo %||% url, ref_part)
+      if (!is.na(ref)) {
+        ref_string <- paste0('git::', repo, '@', ref)
+      } else {
+        ref_string <- paste0('git::', repo)
+      }
     } else if (source == 'url') {
-      paste0('url::', repo %||% url)
+      ref_string <- paste0('url::', repo)
     } else if (source == 'local') {
-      paste0('local::', repo %||% entry$path)
+      ref_string <- paste0('local::', repo)
     } else {
-      cli::cli_abort('Unsupported source {.val {source}} for package {.strong {package}}.')
+      cli::cli_abort('Unsupported source {.val {source}} for package {.strong {pkg}}.')
     }
-  })
+
+    ref_string
+  }, FUN.VALUE = character(1))
 
   if (dry_run) {
     cli::cli_h2('Dry Run: Would install the following {length(pkg_refs)} packages')
